047a68f6379e042fb6f548b8036038d51c6b48ae
irfan87 <nerve2009@yahoo.com>
2019-08-23 23:08:25 +0800

    create a form for add new and update the topics and entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,7 +5,14 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Homepage
     path('', views.index, name='index'),
-    #page that shows all topics
+    # page that shows all topics
     path('topics/', views.topics, name='topics'),
-    path('topics/<int:topic_id>', views.topic, name='topic')
+    # page that shows a topic based on topic_id
+    path('topics/<int:topic_id>', views.topic, name='topic'),
+    # page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # page for edit a current entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry')
 ]
